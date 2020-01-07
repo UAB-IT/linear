37791a4b48b6ac1dc47868a7076262fe09e2f1cf
MattMcAdams <mattmcadams@outlook.com>
2020-01-07 10:53:11 -0600

    Encapsulated tab index logic

--- src/js/interactive-cards.js
+++ src/js/interactive-cards.js
@@ -1,9 +1,9 @@
 // Adds tab index to interactive cards in Widgetkit
-
-var widget = document.getElementsByClassName("interactive-cards")[0];
-var elem = widget.getElementsByClassName("uk-panel");
-    
-for (let i = 0; i < elem.length; i++) {
-  //console.log(elem[i]);
-  elem[i].tabIndex = 0;
+function IT_tabIndex(containerClass = "interactive-cards", elementClass = "uk-panel", debug = false){
+  let container = document.getElementsByClassName(containerClass);
+  let elem = container.getElementsByClassName(elementClass);
+  for (let i = 0; i < elem.length; i++){
+    if (debug === true) {console.log(elem[i]);}
+    elem[i].tabIndex = 0;
+  }
 }
