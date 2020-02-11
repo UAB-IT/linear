00274514083cfe4d6f1fd9e778d590c5c598d714
Matt McAdams <mattmcadams@outlook.com>
2020-02-11 09:47:08 -0600

    Add new banner image CSS
    We're going to eventually move away from loading the banner image from articles with Javascript and use modules instead. This marks code to be removed in the future and adds the new code going forward.
    
    We'll remove the depreciated code when all banner image modules have been created and main article images removed

--- src/js/banner-image.js
+++ src/js/banner-image.js
@@ -6,6 +6,7 @@ function IT_addCSS(css) {
   style.appendChild(document.createTextNode(css));
 }
 
+// DEPRECIATED Implementing banner images with Joomla Modules
 function IT_replaceBanner(debug = false) {
   const mainImg = document.querySelectorAll("div.item-image>img");
     if (debug) { console.log(mainImg[0]); }
