35db605b3244d3721f6e387b5577c1b4241a4b90
Matt McAdams <mattmcadams@outlook.com>
2020-02-13 15:59:44 -0600

    Improve search-list function
    This will allow dynamic search of any collection of tags within a container, hiding them if they don't match the query. If any <dd> or <dt> elements are included, they are hidden or shown together as long as the html follows standard format

--- src/js/search-list.js
+++ src/js/search-list.js
@@ -1,17 +1,40 @@
-function IT_searchList (inputID, containerID, elementTag) {
+function IT_searchList (inputID, containerID, ...tags) {
+
 
   let input = document.getElementById(inputID);
   let container = document.getElementById(containerID);
-  let elements = container.getElementsByTagName(elementTag);
-  // toUpperCase makes sure the search input is not case sensitive
-  let filter = input.value.toUpperCase();
 
-  // Loop through all elements in query, and hide those who don't match the search
-  for (let i = 0; i < elements.length; i++) {
-    if (elements[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
-      elements[i].style.display = "";
-    } else {
-      elements[i].style.display = "none";
+  //console.log('Element tags', tags);
+
+  for ( let i = 0; i < tags.length; i++ ) {
+
+    let elements = container.getElementsByTagName(tags[i]);
+    //console.log('Tags['+i+'] > Elements', elements);
+
+    for ( let i = 0; i < elements.length; i++ ) {
+      let element = elements[i];
+      let previousSibling = element.previousElementSibling;
+      let nextSibling = element.nextElementSibling;
+
+      if ( validate(element, input.value) ) {
+        element.style.display = "";
+      } else if (element.tagName == 'DD' && validate(previousSibling, input.value)) {
+        element.style.display = "";
+      } else if (element.tagName == 'DT' && validate(nextSibling, input.value)) {
+        element.style.display = "";
+      } else {
+        element.style.display = "none";
+      }
     }
   }
+
+
+  function validate( element, filter ) {
+    for ( let i = 0; i < tags.length; i++ ) {
+      if (element.innerHTML.toUpperCase().indexOf(filter.toUpperCase()) > -1 ) {
+        return true;
+      } else { return false; }
+    }
+  }
+
 }
