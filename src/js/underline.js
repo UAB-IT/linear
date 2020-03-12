a738a5ed858b0236f80da1f9554f2811d3cab019
Matt McAdams <mattmcadams@outlook.com>
2020-03-12 17:22:51 -0500

    Update AR related JS

--- src/js/underline.js
+++ src/js/underline.js
@@ -1,31 +1,28 @@
-document.body.className += ' javascript';
+jQuery(document).ready(function () {
+  // Check if element is scrolled into view
+  function isScrolledIntoView (elem) {
+    let docViewTop = jQuery(window).scrollTop()
+    let docViewBottom = docViewTop + jQuery(window).height()
+    let elemTop = jQuery(elem).offset().top
+    let elemBottom = elemTop + jQuery(elem).height()
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop))
+  }
 
-jQuery(document).ready(function() {
-	// Check if element is scrolled into view
-	function isScrolledIntoView(elem) {
-		var docViewTop = jQuery(window).scrollTop();
-		var docViewBottom = docViewTop + jQuery(window).height();
-		var elemTop = jQuery(elem).offset().top;
-		var elemBottom = elemTop + jQuery(elem).height();
-
-		return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
-	}
-
-  jQuery(window).scroll(function() {
-		jQuery('.animated').each(function() {
-			if (isScrolledIntoView(this) === true) {
-				jQuery(this).addClass('fadein');
-			}
-		});
-		jQuery('.underline').each(function() {
-			if (isScrolledIntoView(this) === true) {
-				jQuery(this).addClass('underlineAnim');
-			}
-		});
-		jQuery('.card').each(function() {
-			if (isScrolledIntoView(this) === true) {
-				jQuery(this).addClass('fadein');
-			}
-		});
-	});
-});
+  jQuery(window).scroll(function () {
+    jQuery('.animated').each(function() {
+      if (isScrolledIntoView(this) === true) {
+        jQuery(this).addClass('fadein')
+      }
+    })
+    jQuery('.underline').each(function () {
+      if (isScrolledIntoView(this) === true) {
+        jQuery(this).addClass('underlineAnim')
+      }
+    })
+    jQuery('.card').each(function () {
+      if (isScrolledIntoView(this) === true) {
+        jQuery(this).addClass('fadein')
+      }
+    })
+  })
+})
